5dccffd3c802547e48e909957434931fd2092e98
GIT-AzeK <koladeazariah13@gmail.com>
2022-04-04 14:12:22 -0700

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** Print program name, followed by a new line
+ * without having to comfile the file
+ * argc(argument count) = counts number of arguments.
+ * argv(argument vector) = print the indexes of strings in a 1D-array.
+ *
+ * return 0;
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
