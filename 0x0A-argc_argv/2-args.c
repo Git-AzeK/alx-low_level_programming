588e3434cc58439481bc3bb6bb1be5dbee23949a
GIT-AzeK <koladeazariah13@gmail.com>
2022-04-04 15:19:06 -0700

    f

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-/** Print program name, followed by a new line
- * without having to comfile the file
- * argc(argument count) = counts number of arguments.
- * argv(argument vector) = print the indexes of strings in a 1D-array.
- *
- * return 0;
+/** 
+ * main - Print all arguments received.
+ * @argc: (argument count) = counts number of arguments.
+ * @argv: (argument vector) = print the indexes of strings in a 1D-array.
+ * return: 0 - success.
  */
 
 int main(int argc, char *argv[]){
 
-    int i;
-    for( i = 0; i < argc ; i++ );
-    printf("%s" , *argv[i]);
+    int i = 0;
+    while(i < argc){
+
+    printf("%s\n" , argv[i]);
+
+    }
    
-	return 0;
+	return (0);
 
 }
